f877dda7ab4c6d29b317225038babeae8158b501
Youness Ahasla <unesseah56@gmail.com>
2022-06-16 17:46:57 +0100

    initial commit

---
+++ js/examples.js
@@ -0,0 +1,16 @@
+//Write a program that displays your name and age
+
+console.log('Youness');
+console.log(24);
+
+//Write a program that displays the results of adding, subtracting, multiplying and dividing 6 by 3
+
+console.log(6 + 3);
+console.log(6 - 3);
+console.log(6 * 3);
+
+//Observe the following program and try to predict the values it displays
+
+console.log(4 + 5); // 9
+console.log("4 + 5"); // "4 + 5"
+console.log("4" + "5"); // "45"
